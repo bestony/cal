c1f0948df5c733e71717f8ce01c203b50b8c91e3
HuanCheng Bai <bestony@linux.com>
2022-05-11 00:28:56 +0800

    feat: add tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,22 @@ import cnLocale from "@fullcalendar/core/locales/zh-cn"
 
 export default function Home() {
   return (
-    <div>
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8">
       <Head>
         <title>白宦成的日程安排</title>
       </Head>
+        <h1 className="text-3xl font-bold underline text-center my-8">
+            白宦成的日程安排
+        </h1>
         <FullCalendar
-            initialView="dayGridWeek"
+            // initialView="dayGridWeek"
+            weekNumbers={true}
+
+            headerToolbar={{
+                start: 'today',
+                center: 'title',
+                end: 'prev,next'
+            }}
             locale={cnLocale}
             plugins={[timeGridPlugin,dayGridPlugin, interactionPlugin]}
             nowIndicator={true}
